3a99d483e42eb08aed72d81ce88e623831aa9e07
may <bingqing0822@163.com>
2020-12-03 00:00:20 +0800

    发送短信验证码

---
+++ app/Http/Requests/Api/FormRequest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * 表单请求验证类
+ */
+
+namespace App\Http\Requests\Api;
+
+use Illuminate\Foundation\Http\FormRequest as BaseFormRequest;
+
+class FormRequest extends BaseFormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+}
