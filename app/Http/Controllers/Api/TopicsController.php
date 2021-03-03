ea9fcabecc5b34b6fd592eeac04cb650b51f75ee
may <bingqing0822@163.com>
2021-03-03 11:39:52 +0800

    --话题修改

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -18,4 +18,13 @@ class TopicsController extends Controller
 
         return new TopicResource($topic);
     }
+
+    // 话题修改
+    public function update(TopicRequest $request, Topic $topic)
+    {
+        $this->authorize('update', $topic);
+        $topic->update($request->all());
+
+        return new TopicResource($topic);
+    }
 }
