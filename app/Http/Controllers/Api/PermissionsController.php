e7c6d05762e5a070c1492377873cb99c907319f1
may <bingqing0822@163.com>
2021-03-08 11:57:32 +0800

    --用户权限列表

---
+++ app/Http/Controllers/Api/PermissionsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Resources\PermissionResource;
+
+class PermissionsController extends Controller
+{
+    // 权限列表
+    public function index(Request $request)
+    {
+        $permissions = $request->user()->getAllPermissions();
+        PermissionResource::wrap('data');
+        return PermissionResource::collection($permissions);
+    }
+}
